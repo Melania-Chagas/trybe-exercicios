a6033c0120eddaef7d9ba1a5fc2d95eaaf079cc1
Melania P Chagas <melaniapchagas@gmail.com>
2022-04-11 20:10:29 -0300

    resolvendo o exercicio 3

--- fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javaScript-e-logica-de-programacao/dia-04-objetos-e-funcoes/script.js
+++ fundamentos-do-desenvolvimento-web/bloco-04-introducao-a-javaScript-e-logica-de-programacao/dia-04-objetos-e-funcoes/script.js
@@ -10,3 +10,8 @@ console.log('Bem-vinda,' + info.personagem);
 // exercício 2:
 info.recorrente='sim';
 console.log(info.recorrente);
+
+//exercicio 3:
+for(let key in info){
+    console.log(key);
+}
